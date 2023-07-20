b0364860dd62e53b1ba9d70fc4a8d7e30e3f51b8
Thapelo Magqazana <thapelomagqazana@pop-os.localdomain>
2023-07-20 16:16:08 +0200

    Added generateSecretCode() method and a unittest for it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,20 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.example.numbergenerator.NumberGenerator;
+
+import java.util.List;
+
+/**
+ * Bagels, by Thapelo Magqazana ,tapsmcgzee8@gmail.com
+ * A deductive logic game where you must guess a number based on clues.
+ * This version of this game was inspired by 'Invent Your Own Computer Games with Python'
+ */
 public class Main {
+//    private final int NUM_DIGITS = 3;
+    private final int MAX_GUESSES = 10;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        List<Integer> secretCode = NumberGenerator.generateSecretCode();
+        System.out.println(secretCode);
     }
 }
--- src/main/java/org/example/numbergenerator/NumberGenerator.java
+++ src/main/java/org/example/numbergenerator/NumberGenerator.java
@@ -1,4 +1,27 @@
 package org.example.numbergenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class NumberGenerator {
+    private static final int NUM_DIGITS = 3;
+    private static Random random = new Random();
+
+    public static List generateSecretCode(){
+        List<Integer> secretCode = new ArrayList<>();
+        for (int i = 0; i < NUM_DIGITS; i++){
+            int digit;
+            digit = random.nextInt(10);
+            while (secretCode.contains(digit)){
+                digit = random.nextInt(10);
+            }
+            secretCode.add(digit);
+//            do{
+//                digit = random.nextInt(10);
+//                secretCode.add(digit);
+//            }while (!secretCode.contains(digit));
+        }
+        return secretCode;
+    }
 }
