6130107564c5dcde5c29d7c300c1823e0583f332
Thapelo Magqazana <thapelomagqazana@pop-os.localdomain>
2023-07-20 16:53:38 +0200

    Added getUserInput() to the UserInput class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.numbergenerator.NumberGenerator;
+import org.example.userinput.UserInput;
 
 import java.util.List;
 
@@ -10,11 +11,19 @@ import java.util.List;
  * This version of this game was inspired by 'Invent Your Own Computer Games with Python'
  */
 public class Main {
-//    private final int NUM_DIGITS = 3;
-    private final int MAX_GUESSES = 10;
+    private static boolean isPlaying = true;
+    private static final int MAX_GUESSES = 10;
+    private static int guessesLeft = 10;
 
     public static void main(String[] args) {
         List<Integer> secretCode = NumberGenerator.generateSecretCode();
+
+        while (guessesLeft > 0){
+            int chances = MAX_GUESSES - (guessesLeft - 1);
+            String input = UserInput.getUserInput(chances);
+
+            guessesLeft--;
+        }
         System.out.println(secretCode);
     }
 }
--- src/main/java/org/example/userinput/UserInput.java
+++ src/main/java/org/example/userinput/UserInput.java
@@ -1,4 +1,16 @@
 package org.example.userinput;
 
+import java.util.Scanner;
+
 public class UserInput {
+
+    public static String getUserInput(int chances){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess #"+chances+":");
+        System.out.print("> ");
+        String input = scanner.nextLine();
+
+        return input;
+
+    }
 }
